1f12fb144acfc7c009e54f0868677e7dc5bc402c
Andrew Rachuig <andrew.rachuig@gmail.com>
2022-05-11 16:26:37 -0500

    end of day upload

--- acquire.py
+++ acquire.py
@@ -1,7 +1,10 @@
 import env
 import os
+import pandas as pd
 
-# The following function can be used to create or access a local cache of data
+
+'''
+# The following function is an example of how a function can be used to create and/or access a local cache of data
 
 def get_date_example(): #this can be any file type, not just csv
     filename = "example.csv"
@@ -19,3 +22,58 @@ def get_date_example(): #this can be any file type, not just csv
 
         #returns the df to the calling code to be used
         return df
+'''
+
+
+def get_titanic_data():
+    filename = "titanic.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        query = '''
+        SELECT *
+        FROM passengers;        
+        '''
+        url = env.get_db_url('titanic_db')
+        df = pd.read_sql(query, url)
+        df.to_csv(filename)
+
+        return df  
+
+def get_iris_data():
+    filename = "iris.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        query = '''
+        SELECT *
+        FROM measurements
+        JOIN
+        species USING(species_id);      
+        '''
+        url = env.get_db_url('iris_db')
+        df = pd.read_sql(query, url)
+        df.to_csv(filename)
+
+        return df  
+
+def get_telco_data():
+    filename = "telco.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        query = '''
+        SELECT * 
+        FROM customers
+        JOIN contract_types USING (contract_type_id)
+        JOIN payment_types USING (payment_type_id)
+        JOIN internet_service_types USING (internet_service_type_id);     
+        '''
+        url = env.get_db_url('telco_churn')
+        df = pd.read_sql(query, url)
+        df.to_csv(filename)
+
+        return df  
