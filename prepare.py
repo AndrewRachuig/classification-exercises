f10db5fc53507d4569db48310f6ba7dc4b9361ae
Andrew Rachuig <andrew.rachuig@gmail.com>
2022-05-13 17:00:04 -0500

    updated end of day

--- prepare.py
+++ prepare.py
@@ -1,10 +1,18 @@
 import acquire
 import pandas as pd
+import numpy as np
+# train test split from sklearn
+from sklearn.model_selection import train_test_split
+# imputer from sklearn
+from sklearn.impute import SimpleImputer
 
 
 def prep_iris(iris):
     '''
-    Takes in a dirty iris dataframe and outputs a cleaned/transformed iris dataframe as per the requirements in the exercises.
+    Takes in a dirty iris dataframe and returns a cleaned/transformed iris dataframe as per the requirements in the exercises.
+    Arguments: iris - a pandas dataframe with the expected feature names and columns
+    Return: clean_iris - a dataframe with the requisite transformations applied to it.
+    The returned Dataframe is not yet split for train, validate, test.
     '''
     iris.drop(columns = ['Unnamed: 0', 'species_id', 'measurement_id'], inplace=True)
     iris.rename(columns = {'species_name': 'species'}, inplace=True)
@@ -48,4 +56,24 @@ def prep_titanic_data(df):
     train, validate, test = impute_age_titanic(train, validate, test)
     return train, validate, test
 
+
+def prep_telco(telco):
+    '''
+    Takes in a dirty telco dataframe and returns a cleaned/transformed telco dataframe
+    Arguments: telco - a pandas dataframe with the expected feature names and columns
+    Return: clean_telco - a dataframe with various cleaning operations performed on it.
+    
+    '''
+    telco.drop(columns = ['Unnamed: 0', 'customer_id', 'internet_service_type_id', 'payment_type_id', 'contract_type_id', 'phone_service'], inplace=True)
+    telco.drop_duplicates(inplace=True)
+    telco.total_charges.replace(' ', '0', inplace=True)
+    telco['total_charges'] = pd.to_numeric(telco['total_charges'], errors='coerce')
+    dummies_list = telco.select_dtypes('object').columns
+    dummy_df = pd.get_dummies(telco[dummies_list], dummy_na=False, drop_first=[True, True])
+    telco = pd.concat([telco, dummy_df], axis = 1)
+    
+    # Before splitting the resulting cleaned dataframe into Train, Validate, Test subsets I would need to drop the 
+    # original categorical columns used to create the dummy categorical columns. That could be done here before 
+    # returning the cleaned telco dataframe.
+    return telco
 
